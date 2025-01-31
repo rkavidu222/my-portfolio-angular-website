a1197ee871468350729063bb9c2aad3a70c91855
rkavidu222 <123630912+rkavidu222@users.noreply.github.com>
2025-01-31 11:58:09 +0530

    31/01/2025 third commit

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,18 +1,20 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
 
+  // Method to open your LinkedIn profile
   goToLinkedin() {
-    window.open('https://www.linkedin.com/in/sebastiano-fazzino/', '_blank');
+    window.open('https://www.linkedin.com/in/kavindu-rashmika-wasala-arachchi/', '_blank');
   }
 
+  // Getter for your email address
   get email(): string {
-    return 'sebastianofazzino92@gmail.com';
+    return 'rkavidu222@gmail.com';
   }
 }
